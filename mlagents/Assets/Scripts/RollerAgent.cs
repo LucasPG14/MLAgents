56d345ac5f2f2956389e3e58b81f522ba48de798
Oriol Bernal Martinez <youtfrost@gmail.com>
2021-12-01 09:57:20 +0100

    MLTAgents
    MLTAgents implemented
    
    Co-Authored-By: Lucas Pérez <lucaspg14@gmail.com>

---
+++ mlagents/Assets/Scripts/RollerAgent.cs
@@ -0,0 +1,20 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        base.OnEpisodeBegin();
+    }
+}
